160fdcd9396fe14942eb93d7ca9c0ee9d5927710
irinadeeva <deeva97@me.com>
2024-11-04 12:46:55 +0300

    Initial Commit

---
+++ Task2_Buttons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task2_Buttons
+//
+//  Created by Irina Deeva on 04/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
